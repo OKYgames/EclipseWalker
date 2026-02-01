0996741ef56a683152227caaa12e0b9fd48f7332
yangtaewoong <tom0323@naver.com>
2026-02-01 19:38:43 +0900

    .

--- EclipseWalker/FrameResource.h
+++ EclipseWalker/FrameResource.h
@@ -22,8 +22,6 @@ struct MaterialConstants
     DirectX::XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
     DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
     float Roughness = 0.25f;
-
-
 };
 
 struct PassConstants
