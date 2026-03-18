201c3fce351b6c8fc672d718e57837d7f826dfb7
ohgarane <ohgarane8@naver.com>
2026-03-18 21:33:27 +0900

    주석정리

--- Eclipse Walker Server/ServerPacketHandler.cpp
+++ Eclipse Walker Server/ServerPacketHandler.cpp
@@ -1,5 +1,5 @@
 #include "ServerPacketHandler.h"
-#include "Session.h" // 여기서 Session을 include 해야 함
+#include "Session.h"
 #include "GlobalQueue.h"
 #include "Room.h"
 #include "DBConnection.h"
@@ -12,7 +12,6 @@ void ServerPacketHandler::HandlePacket(std::shared_ptr<Session> session, BYTE* b
     {
     case PacketID::C_LOGIN:
     {
-        // 버퍼 안전성 체크 (패킷 크기만큼 충분한지)
         if (len < sizeof(PKT_C_LOGIN)) break;
 
         PKT_C_LOGIN* pkt = reinterpret_cast<PKT_C_LOGIN*>(buffer);
@@ -46,25 +45,21 @@ void ServerPacketHandler::HandlePacket(std::shared_ptr<Session> session, BYTE* b
 
 void ServerPacketHandler::Handle_C_LOGIN(std::shared_ptr<Session> session, PKT_C_LOGIN& pkt)
 {
-    // 데이터를 복사해서 람다 안에 캡처
     PKT_C_LOGIN pktCopy = pkt;
 
     G_JobQueue->Push([session, pktCopy]()
         {
             std::cout << "[Logic Thread] Login Request Process..." << std::endl;
 
-            // 1. char[50] 배열을 바로 std::string으로 변환 (에러 해결)
             std::string inputId = pktCopy.id;
             std::string inputPw = pktCopy.password;
             int userUid = 0;
 
-            // 2. DB 검증 시작
             bool isLoginSuccess = DBConnection::GetInstance()->Login(inputId, inputPw, userUid);
 
-            // 3. 클라이언트에게 보낼 응답 패킷 세팅
             PKT_S_LOGIN sendPkt;
             sendPkt.header.size = sizeof(PKT_S_LOGIN);
-            sendPkt.header.id = PacketID::S_LOGIN; // 본인 코드의 PacketID 네이밍에 맞게 확인하세요
+            sendPkt.header.id = PacketID::S_LOGIN; 
 
             if (isLoginSuccess == true)
             {
