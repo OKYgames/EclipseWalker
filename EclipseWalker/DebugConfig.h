36c5cec23733fa04ea43f0e71a8ad6bc57c30b60
B-zin-KM <165834109+B-zin-KM@users.noreply.github.com>
2026-07-26 21:05:40 +0900

    끝

--- EclipseWalker/DebugConfig.h
+++ EclipseWalker/DebugConfig.h
@@ -3,13 +3,13 @@
 namespace DebugConfig
 {
     constexpr bool kEnableBackendConnection = true;
-    constexpr bool kEnableDbLogin = false;
+    constexpr bool kEnableDbLogin = true;
     constexpr bool kAllowSoloLobbyStart = true;
     constexpr int kOfflineStartStage = 1;
     constexpr bool kDebugFullLanternGauge = false;
 
     constexpr const char* kServerIp = "127.0.0.1";
-    //constexpr const char* kServerIp = "112.170.181.135";
+    //constexpr const char* kServerIp = "121.190.132.245";
     constexpr short kServerPort = 7777;
     constexpr const char* kDebugLoginId = "debug_user";
     constexpr const char* kDebugLoginPassword = "debug_pw";
