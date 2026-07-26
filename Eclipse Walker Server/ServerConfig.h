4b18a74cb3b91af29d2cca92179737f883f26b40
B-zin-KM <165834109+B-zin-KM@users.noreply.github.com>
2026-07-26 21:27:40 +0900

    진짜 끝

--- Eclipse Walker Server/ServerConfig.h
+++ Eclipse Walker Server/ServerConfig.h
@@ -3,7 +3,7 @@
 namespace ServerConfig
 {
     constexpr bool kEnableDbLogin = true;
-    constexpr bool kEnableDebugLogin = true;
+    constexpr bool kEnableDebugLogin = false;
     constexpr const char* kDebugLoginId = "debug_user";
     constexpr const char* kDebugLoginPassword = "debug_pw";
 }
